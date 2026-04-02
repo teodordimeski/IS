2f9c78192f1e542bd736eaaac511ac668bd15093
teodordimeski <teodordimeski@gmail.com>
2026-04-02 12:03:40 +0200

    added controller methods post put

--- Consultations Application/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ Consultations Application/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173e28646e06b95117f95675705c912bc848b4d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
